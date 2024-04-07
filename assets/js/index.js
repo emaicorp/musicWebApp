3c37f5ac7687e3e89b4728bdb535ba216fad93ac
emaicorp <emaicorp@gmail.com>
2024-04-07 01:00:30 +0100

    added the play button and the animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
+    // nav bar scroll fuction to blur mode
+    window.addEventListener("scroll", function(){
+        var nav = this.document.querySelector("nav");
+        nav.classList.toggle("sticky", window.scrollY>0)
+    })
+    // list of music genre to select from
   let musicGenres = [
     "Pop music genre",
     "Hip hop music genre",
@@ -26,9 +32,9 @@ window.addEventListener("DOMContentLoaded", () => {
     "Merengue music genre",
     "Bachata music genre",
   ];
+//   creating and appending list items to parent dom
   let ul = document.querySelector('ul');
   musicGenres.forEach((genre) => {
-    console.log(genre);
     let li = document.createElement('li');
     let hr = document.createElement("hr");
     li.setAttribute('class', 'genre-li');
@@ -36,4 +42,20 @@ window.addEventListener("DOMContentLoaded", () => {
     ul.appendChild(li);
     ul.appendChild(hr);
   });
+//   creating move over effect over the cards
+let cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    let button = card.querySelector('.buttons');
+
+    card.addEventListener('mouseover', (e) => {
+        button.classList.add('show'); 
+    });
+
+    card.addEventListener('mouseout', (e) => {
+        button.classList.remove('show'); 
+    });
+});
+
+
 });
